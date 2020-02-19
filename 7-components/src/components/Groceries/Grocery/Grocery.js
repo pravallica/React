03d347e2fae96680e53f39d872b7ae8a02c8682a
pravallica <mpravallica@gmail.com>
2020-02-18 19:20:59 -0800

    HOC

--- 7-components/src/components/Groceries/Grocery/Grocery.js
+++ 7-components/src/components/Groceries/Grocery/Grocery.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+// import Aux from '../../../hoc/Aux';
 import classes from './Grocery.module.css'
+import WithClass from '../../../hoc/WithClass';
 
     
 class Grocery extends Component{
     render(){
     console.log('Grocery.js... Rendering');
     return(
-        <div className= {classes.Grocery}>
+        // <Aux>
+        <WithClass classes= {classes.Grocery}>
         <p>The item name is {this.props.items } and price is { this.props.price }</p>
-        </div>
+       
+        </WithClass>
+        // </Aux>
     );
 }
 } 
