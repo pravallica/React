275f4e2d3f62be0f76966678e40485fa048dd085
pravallica <mpravallica@gmail.com>
2020-03-04 16:53:33 -0800

    Redux

---
+++ redux/redux-basics.js
@@ -0,0 +1,39 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const initialState = {
+    counter:0
+}
+
+
+//Reducer
+const rootReducer = (state = initialState, action) =>{
+
+    if(action.type === 'INC_COUNTER'){
+       return { ...state, 
+        counter: state.counter + 1
+       }
+    }
+    else if(action.type === 'ADD_COUNTER'){
+        return { 
+            ...state, 
+         counter: state.counter + action.value
+        }
+     }
+    return state
+    
+};
+
+//Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//Subscription
+store.subscribe(()=>{
+    console.log('Sub',store.getState());
+})
+
+//Dispatching Actions
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 5});
+console.log(store.getState())
+
